94192eeb80a6495d4e09bc695364277ecf069e43
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-11 08:37:46 -0700

    1st commit

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import sys
 
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
+chain = 0
 
 if len(num1) > len(num2):
 	print ('False')
-	sys.exit(0)
+else:
+	for i in range(len(num2)):
+		for j in range(len(num1)):
+			if num2[i+j] == num1[j]:
+				chain += 1
+	if chain == len(num1):
+		print("True")
+	else:
+		print("False")
 # ******************************
 # Make your Code
 # ******************************
